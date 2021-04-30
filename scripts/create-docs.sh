99ef8773f4a99b0ed6db7d50fc725d8f09c8e666
Paul Wright <pwright@redhat.com>
2021-04-30 12:38:59 +0100

    simplify

--- scripts/create-docs.sh
+++ scripts/create-docs.sh
@@ -1,8 +1,8 @@
 
 # requires 'npm i -g replace-in-file'
 for example in standalone policy mesh-single-cluster mesh-multi-cluster broker-single-cluster; do
-cp -r $example/* modules/$example/examples
-replace-in-file modules/$example/examples/* --configFile=scripts/config.js
+cp -r $example modules/ROOT/examples/
+replace-in-file modules/ROOT/examples/$example/* --configFile=scripts/config.js
 done
 
 antora preview-playbook.yml
